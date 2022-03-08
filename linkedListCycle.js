542883007e680420ca4ef2c1d428033424ac9eb8
Abhishek Singh <58261248+abhishek-singh77@users.noreply.github.com>
2022-03-08 23:34:46 +0530

    linked list cycle

---
+++ linkedListCycle.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    let start = new ListNode(head);
+    while(head){
+        if(head.val === 'X'){
+            return true;
+        }
+        head.val = 'X';
+        head = head.next;
+    }
+    return false;
+};
